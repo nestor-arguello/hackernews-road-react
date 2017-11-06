12f33d09a959b642a50833514e84b4d9594b847a
Néstor Argüello <nestor.arguello@gmail.com>
2017-11-06 02:57:29 -0400

    Controlled Components

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ class App extends Component {
     this.setState({ searchTerm: event.target.value })
   }
   render() {
+    const { list, searchTerm } = this.state;
     return (
       <div className="App">
         <form>
@@ -63,7 +64,7 @@ class App extends Component {
             onChange={this.onSearchChange}
           />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map( item =>
+        {list.filter(isSearched(searchTerm)).map( item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
